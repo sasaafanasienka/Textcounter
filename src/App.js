e4d428e0c3e13b77c13ca492e8a85774344ca40f
sasaafanasienka <mark0182@tut.by>
2021-02-06 15:26:29 +0300

    Redirect to Non-exist pages added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './App.scss';
-import MainContent from './components/Main/Main';
 import AboutAuthor from './components/AboutAuthor/AboutAuthor';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import Main from './components/Main/Main';
 import Header from './components/Header/Header';
 
@@ -27,6 +26,10 @@ function App() {
 
           <Route path='/about'>
             <AboutAuthor />
+          </Route>
+
+          <Route path="*">
+            <Redirect to="/" />
           </Route>
       </div>
     </ThemeContext.Provider>
