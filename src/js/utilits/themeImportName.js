ff4285038a105e5ab25189996eb730f7565e87b3
sasaafanasienka <mark0182@tut.by>
2021-02-10 19:00:38 +0300

    удалены неиспользуемые утилиты

--- src/js/utilits/themeImportName.js
+++
@@ -1,7 +0,0 @@
-export function themeImportName(themeProperties, baseClassName) {
-    if (themeProperties.id === 0) {
-        return baseClassName
-    } else {
-        return `${baseClassName} ${baseClassName}${themeProperties.modifierClass}`
-    }
-}
